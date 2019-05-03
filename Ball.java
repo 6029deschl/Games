4237aae2d41b000df32a7194887c4383ef8bde5e
6029deschl <36146390+6029deschl@users.noreply.github.com>
2019-05-03 11:40:57 -0500

    Add files via upload

---
+++ Ball.java
@@ -0,0 +1,16 @@
+
+public class Ball {
+	int x, y, z;
+
+	// ball variable
+	// starting position(x y z points)
+	// kicked when mouse clicked
+	// reset after decided if goal or not
+	public void kickBall() {
+		for (int i = z; i > 0; i--) {
+			// ball moves forward
+			// if(ball goes in goal)
+			// reset();
+		}
+	}
+}
